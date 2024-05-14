14937a464998b59ced5413bafe8e12f292057769
marcocuellar7293@gmail.com <marcocuellar7293@gmail.com>
2024-05-14 10:58:14 -0400

    asdasd

--- src/main/java/com/sisu/sisu/controller/FichaSisuController.java
+++ src/main/java/com/sisu/sisu/controller/FichaSisuController.java
@@ -497,8 +497,8 @@ public class FichaSisuController {
 		try {
 			request1.put("usuario", codigoAdministrativo);
 
-			//String url = "http://localhost:3333/api/londraPost/v1/personaLondra/obtenerDatos";
-			String url = "http://virtual.uap.edu.bo:7174/api/londraPost/v1/personaLondra/obtenerDatos";
+			String url = "http://172.16.21.2:3333/api/londraPost/v1/obtenerDatos";
+			//String url = "http://virtual.uap.edu.bo:7174/api/londraPost/v1/personaLondra/obtenerDatos";
 
 			HttpHeaders headers = new HttpHeaders();
 			headers.setContentType(MediaType.APPLICATION_JSON);
